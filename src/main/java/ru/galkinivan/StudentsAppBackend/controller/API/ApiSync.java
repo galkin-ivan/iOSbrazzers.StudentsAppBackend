5476422c2522b7c702929da885fb333263b378b8
galkin-ivan <galkinivansergeevich@gmail.com>
2017-12-19 04:33:12 +0300

    Made basic sync response struct

--- src/main/java/ru/galkinivan/StudentsAppBackend/controller/API/ApiSync.java
+++ src/main/java/ru/galkinivan/StudentsAppBackend/controller/API/ApiSync.java
@@ -44,6 +44,9 @@ public class ApiSync {
     public String doSync( @RequestParam(value = "data") String data){
         System.out.println(data);
         JSONObject fullSyncData;
+        JSONObject returnSyncData = new JSONObject();
+        JSONObject returnedDueToIDs = new JSONObject();
+        JSONObject returnedToSync = new JSONObject();
         try {
             fullSyncData = (JSONObject) (new JSONParser()).parse(data);
         }
@@ -52,6 +55,13 @@ public class ApiSync {
             return "{ \"error\" : \"true\" }";
         }
 
+        //JSON Arrays with data to be returned due to being new so that client has all the IDs...
+        JSONArray tasksForClientDueToIDs = new JSONArray();
+        JSONArray teachersForClientDueToIDs = new JSONArray();
+        JSONArray timeTableEventsForClientDueToIDs = new JSONArray();
+        JSONArray activitiesForClientDueToIDs = new JSONArray();
+        JSONArray subjectsForClientDueToIDs = new JSONArray();
+
         Long groupIDLong = Long.parseLong(fullSyncData.get("groupID").toString());
         studyGroup = groupDao.findOne(groupIDLong);
         JSONObject toServerData = (JSONObject) fullSyncData.get("toServerData");
@@ -59,79 +69,86 @@ public class ApiSync {
 
         for (Object task : tasksFromClient){
             if (task instanceof JSONObject){
-                handleTaskFromClient((JSONObject) task);
+                Long newID = handleTaskFromClient((JSONObject) task);
+                if(newID != null){
+                    ((JSONObject) task).put("id", newID);
+                    tasksForClientDueToIDs.add(task);
+                    System.out.println("New task id "+tasksForClientDueToIDs.toJSONString());
+                }
             }
         }
 
-        JSONObject initialData = new JSONObject();
+        //Put returned due to ids data to return struct
+        returnedDueToIDs.put("tasks", tasksForClientDueToIDs);
+        returnedDueToIDs.put("teachers", teachersForClientDueToIDs);
+        returnedDueToIDs.put("timeTableEvents", timeTableEventsForClientDueToIDs);
+        returnedDueToIDs.put("activities", activitiesForClientDueToIDs);
+        returnedDueToIDs.put("subjects", subjectsForClientDueToIDs);
+        returnSyncData.put("returnedDueToNewIDs", returnedDueToIDs);
 
-        // add subjects to initialData
-        JSONArray jsonSubjects = new JSONArray();
-
-        initialData.put("subjects", jsonSubjects);
-
-        // add teachers to initialData
-        JSONArray jsonTeachers = new JSONArray();
-
-        initialData.put("teachers", jsonTeachers);
-
-        // add activities to initialData
-        JSONArray jsonActivities = new JSONArray();
-
-        initialData.put("activities", jsonActivities);
-
-        // add tasks to initialData
-        JSONArray jsonTasks = new JSONArray();
-
-        initialData.put("tasks", jsonTasks);
-
-        // add time table events to initialData
-        JSONArray jsonEvents = new JSONArray();
-
-        initialData.put("timeTableEvents", jsonEvents);
-
-        return initialData.toJSONString();
+        //Put other data to return struct
+        //TODO Add data to returnedToSync
+        returnSyncData.put("returnedToSync", returnedToSync);
+        System.out.println(returnSyncData.toJSONString());
+        return returnSyncData.toJSONString();
     }
 
-    private void handleTaskFromClient(JSONObject task){
+    //Returns not null if task is new and heeds to be returned to client with real ID
+    private Long handleTaskFromClient(JSONObject task){
+        Long toReturn = null;
         System.out.println(Long.parseLong(task.get("id").toString()));
-        if(Integer.parseInt(task.get("id").toString()) != 0){
-
-            String dateString = task.get("date").toString();
-            DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
-            Date date = null;
-            try {
-                 date = format.parse(dateString);
-            }
-            catch (java.text.ParseException pe){
-                System.out.println("HandleTaskFromClient :: date parse exception");
-            }
-
-            String description = task.get("description").toString();
-            String shortName = task.get("shortName").toString();
-            Integer status = Integer.parseInt(task.get("status").toString());
-            Integer priority = Integer.parseInt(task.get("priority").toString());
-            String subjectString = task.get("subject").toString();
-
-            Task taskDBObject = taskDao.findOne(Long.parseLong(task.get("id").toString()));
-            taskDBObject.setDescription(description);
-            taskDBObject.setShortName(shortName);
-            taskDBObject.setStatus(status);
-            taskDBObject.setPriority(priority);
-            taskDBObject.setDate(date);
-            taskDBObject.setGroup(studyGroup);
-            Subject subject;
-            if(doesSubjectExists(subjectString)){
-                subject = subjectDao.findByName(subjectString);
-            }else {
-                subject = new Subject();
-                subject.setName(subjectString);
-                subjectDao.save(subject);
-            }
-            taskDBObject.setSubject(subject);
-
-            taskDao.save(taskDBObject);
+        //Prepare data
+        String dateString = task.get("date").toString();
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        Date date = null;
+        try {
+            date = format.parse(dateString);
         }
+        catch (java.text.ParseException pe){
+            System.out.println("HandleTaskFromClient :: date parse exception");
+        }
+        String description = task.get("description").toString();
+        String shortName = task.get("shortName").toString();
+        Integer status = Integer.parseInt(task.get("status").toString());
+        Integer priority = Integer.parseInt(task.get("priority").toString());
+        String subjectString = task.get("subject").toString();
+
+        Task taskDBObject;
+        //Check if task is new or not
+        if(Integer.parseInt(task.get("id").toString()) != 0){
+            toReturn = null;
+            taskDBObject = taskDao.findOne(Long.parseLong(task.get("id").toString()));
+        }
+        else{
+            //The case when a task is not present on the server
+            //TODO Check if that task does actiualy present
+            taskDBObject = new Task();
+            toReturn = new Long(0);
+        }
+
+        taskDBObject.setDescription(description);
+        taskDBObject.setShortName(shortName);
+        taskDBObject.setStatus(status);
+        taskDBObject.setPriority(priority);
+        taskDBObject.setDate(date);
+        taskDBObject.setGroup(studyGroup);
+        Subject subject;
+        if(doesSubjectExists(subjectString)){
+            subject = subjectDao.findByName(subjectString);
+        }else {
+            subject = new Subject();
+            subject.setName(subjectString);
+            subjectDao.save(subject);
+        }
+        taskDBObject.setSubject(subject);
+
+        taskDao.save(taskDBObject);
+
+        if(toReturn != null){
+            toReturn = taskDBObject.getId();
+        }
+
+        return toReturn;
     }
 
     private boolean doesSubjectExists(String subjectName){
